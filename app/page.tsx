3800859371a8f2e0fb36673acd3b96a88e4d5674
Miroslav Dimanov <miroslav.dimanov86@gmail.com>
2024-03-18 13:38:15 +0200

    main page server component removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-//Remove "use client" used for development purpose remove when ready
 import Link from "next/link";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
